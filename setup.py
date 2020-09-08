3bb8873025382bd90d6899d83c347602d87b8a08
giangic <ghimotcaiten@gmail.com>
2020-09-08 09:57:17 +0700

    Twisted library python 3.7 fix async error

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='twisted',
+    name='Twisted',
     version='0.1.0',
     url='',
     description='',
     packages=find_packages(),
-    dependency_links=['https://github.com/NMTruongGiang/twisted_py37.git#egg=twisted']
+    dependency_links=['https://github.com/NMTruongGiang/twisted_py37.git#egg=Twisted']
 )
